493027ecb29c8bd0cb8458664f762f826f898889
Brian Santos <142011551+briansantoss@users.noreply.github.com>
2024-11-13 11:03:43 -0300

    Melhorando o cadastro de produtos

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
 import React from 'react';
 import ProdutoListar from './components/pages/produto/ProdutoListar';
 import ProdutoCadastro from './components/pages/produto/ProdutoCadastro';
+import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
+import ConsultarCEP from './components/samples/ConsultarCEP';
 
 function App() {
   return (
-    <div>
-      <ProdutoCadastro />
-      <ProdutoListar />
+    <div className="App">
+      <BrowserRouter>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/pages/produto/listar">Listar Produtos</Link>
+          </li>
+          <li>
+            <Link to="/pages/produto/cadastrar">Cadastrar Produtos</Link>
+          </li>
+          <li>
+            <Link to="/pages/endereço/consultar">Consultar Endereço</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<ProdutoListar />} />
+        <Route path="/pages/produto/listar" element={<ProdutoListar />} />
+        <Route path="/pages/produto/cadastrar" element={<ProdutoCadastro />} />
+        <Route path="/pages/endereco/consultar" element={<ConsultarCEP />} />
+      </Routes>
+      </BrowserRouter>
     </div>
   )
 }
--- src/components/pages/produto/ProdutoCadastro.tsx
+++ src/components/pages/produto/ProdutoCadastro.tsx
@@ -1,11 +1,19 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { Produto } from "../../../models/Produto"
+import { Categoria } from "../../../models/Categoria"
 
 function ProdutoCadastro() {
     const [nome, setNome] = useState("")
     const [descricao, setDescricao] = useState("")
     const [quantidade, setQuantidade] = useState("")
     const [preco, setPreco] = useState("")
+    const [categorias, setCategorias] = useState<Categoria[]>([])
+
+    useEffect(() => {
+        fetch(" http://localhost:5262/categoria/listar")
+        .then(resposta => resposta.json())
+        .then(categorias => setCategorias(categorias))
+    });
 
     function enviarProduto(e: any) {
         e.preventDefault()
@@ -20,7 +28,7 @@ function ProdutoCadastro() {
         fetch("http://localhost:5262/produto/cadastrar", {
             method: "POST",
             headers: {
-                "Content-type": "application/json"
+                "Content-Type": "application/json"
             },
             body: JSON.stringify(produto)
         })
@@ -38,7 +46,7 @@ function ProdutoCadastro() {
             <form onSubmit={enviarProduto}>
                 <div>
                     <label htmlFor="nome-produto">Nome: </label>
-                    <input type="text" name="nome-produto" onChange={(e: any) => setNome(e.target.value)}/>
+                    <input type="text" name="nome-produto" onChange={(e: any) => setNome(e.target.value)} required />
                 </div>
                 <div>
                     <label htmlFor="descricao">Descrição: </label>
@@ -46,13 +54,21 @@ function ProdutoCadastro() {
                 </div>
                 <div>
                     <label htmlFor="quantidade-produto">Quantidade: </label>
-                    <input type="number" name="quantidade-produto" min="1" onChange={(e: any) => setQuantidade(e.target.value)}/>
+                    <input type="number" name="quantidade-produto" min="1" onChange={(e: any) => setQuantidade(e.target.value)} required />
                 </div>
                 <div>
                     <label htmlFor="preco-produto">Preço: </label>
-                    <input type="number" name="preco-produto" min="1" onChange={(e: any) => setPreco(e.target.value)}/>
+                    <input type="number" name="preco-produto" min="1" onChange={(e: any) => setPreco(e.target.value)} required />
                 </div>
                     <input type="submit" value="Cadastrar"/>
+
+                <div>
+                    <select onChange={(e: any) => console.log(e)}>
+                        {categorias.map(c => (
+                            <option value={c.id} key={c.id}>{c.nome}</option>
+                        ))}
+                    </select>
+                </div>
             </form>
         </div>
         
